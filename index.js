c08261b0d425692fe46533a69d8d7d7d68c6b1a3
Quantium <andres@quantium.com.mx>
2019-09-17 16:48:34 -0500

    Temporary solution to set the right listeners after moving scraping functionality to a forked process

--- index.js
+++ index.js
@@ -12,38 +12,45 @@ const MongoClient = require('mongodb').MongoClient;
     var coso = await cursor.next();
     console.log("coso", coso.codigoEAN1);
     var doc = new SanPabloDoc(coso.codigoEAN1);
-    const ready = async html => {
-        console.log("---->", typeof await doc.price, await doc.price);
-        if (await doc.price < 999999) {
-            try {
-                scraper.updateOne({ ean: await doc.ean }, { "$set": { prices: { sanpablo: await doc.price } } }, { upsert: true },
-                    async function(err, data) {
-                        if (err) {
-                            console.log(err);
-                            process.exit(1);
-                            return;
-                        }
-                        console.log("saved", doc.ean, doc.price);
 
-                        if (cursor.hasNext) {
-                            let next = await cursor.next();
-                            if (next.codigoEAN1) {
-                                doc.removeAllListeners('ready');
-                                doc = new SanPabloDoc(next.codigoEAN1.toString());
-                                doc.on('ready', ready);
-                            } else {
-                                console.log("ERROR", next.codigoEAN1);
-                            }
+    const ready = async html => {
+        if (await doc.price < 999999) {
+            scraper.updateOne({ ean: await doc.ean }, { "$set": { prices: { sanpablo: await doc.price } } }, { upsert: true },
+                async function(err, data) {
+                    if (err) {
+                        console.log(err);
+                        process.exit(1);
+                        return;
+                    }
+                    console.log("Saved", doc.ean, doc.price);
+                    if (cursor.hasNext) {
+                        let next = await cursor.next();
+                        if (next.codigoEAN1) {
+                            doc.removeAllListeners('ready');
+                            doc = new SanPabloDoc(next.codigoEAN1.toString());
+                            doc.on('ready', ready);
+                        } else {
+                            console.log("ERROR", next.codigoEAN1);
                         }
                     }
-                );
-            } catch (e) {
-                console.log(e);
-                process.exit(1);
-                return;
-            }
+                }
+            );
 
+        } else {
+            console.log("No price. No saved", doc.ean, doc.price);
+            //Hate to do this, repeating code I mean, but it's temporary
+            if (cursor.hasNext) {
+                let next = await cursor.next();
+                if (next.codigoEAN1) {
+                    doc.removeAllListeners('ready');
+                    doc = new SanPabloDoc(next.codigoEAN1.toString());
+                    doc.on('ready', ready);
+                } else {
+                    console.log("ERROR", next.codigoEAN1);
+                }
+            }
         }
     };
     doc.on('ready', ready);
+
 })();
