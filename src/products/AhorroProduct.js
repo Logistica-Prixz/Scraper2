6a262d3fda2282604268afb13b0aeae05922a79e
Quantium <andres@quantium.com.mx>
2019-10-31 10:19:26 -0600

    Solving a terrible bug that stops Farmalisto and Ahorro to scraping

--- src/products/AhorroProduct.js
+++ src/products/AhorroProduct.js
@@ -3,7 +3,7 @@ module.exports = class AhorroProduct extends AbstractProduct {
 
     get price() {
         if (this.$) {
-            var el = this._cheerio.text(this.$('.regular-price .price')).trim().match(/[0-9]+,?[0-9]*\.?[0-9]+/g);
+            var match = this._cheerio.text(this.$('.regular-price .price')).trim().match(/[0-9]+,?[0-9]*\.?[0-9]+/g);
             if (match) {
                 return parseFloat(match[0].split(',').join(''));
             }
